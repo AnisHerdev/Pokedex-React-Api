84f24b9079b3097093037983b84936646bb614c6
AnisHerdev <88375763+AnisHerdev@users.noreply.github.com>
2025-01-10 00:15:17 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react'
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [data, setData] = useState();
+  const [number, setNumber] = useState(1);
+  var URL=`https://pokeapi.co/api/v2/pokemon/${number}`
+  var num;
+  useEffect(()=>{
+    axios.get(URL).then((response) => {
+      setData(response.data);
+      console.log(response.data)
+    }).catch((err)=>{
+      window.alert(err);
+      console.log(URL);
+    })
+  },[URL])
+  return (
+    <>
+    <br></br>
+      <h1 className='heading'>Pokemon Viewer</h1><br></br>
+      <center>
+      <input type='number' placeholder="Number" onChange={(e)=>{setNumber(e.target.valueAsNumber)}}></input>
+      <p className='display'>{data?data.name:"null"}</p>
+      <img src={data?data.sprites.other.dream_world.front_default:"/src/assets/loading.webp"} />
+      </center>
+    </>
+  )
+}
+export default App
